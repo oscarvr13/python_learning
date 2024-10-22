75d412d0ff6dbc57f0883b0b062224ce4eaabf48
Oscar Vazquez <oscarvr1213@gmail.com>
2024-10-21 20:37:14 -0600

    Revert "Add all python files"
    This reverts commit d6fb8dfe1df4e13f51ab7701e8373237cdfcb8b9.

--- Day2/Proyect_Day2.py
+++
@@ -1,8 +0,0 @@
-name  = input("Cual es tu nombre?: ")
-sells = input("How much is your sells?: ")
-
-percentage_sell = 0.13
-
-total_comision = round(float(sells) * percentage_sell, 2)
-
-print(f"Seller: {name} and comission is ${total_comision}")
--- Day8/Project/tools/decorators.py
+++
@@ -1,9 +0,0 @@
-def decorar_turno(next_function):
-    def agreagar_saludo():
-        resultado = next_function()
-        print(f"Hola su turno es {resultado}")
-        print(f"Por favor espere")
-    return agreagar_saludo
-
-
-
